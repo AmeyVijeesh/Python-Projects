0ef3562820ae8718df13c196d6aa7ca0163b100d
•AV• <74102654+AmeyVijeesh@users.noreply.github.com>
2021-08-03 19:41:22 +0530

    Create main.py

---
+++ Tkinter: Online Image opener/main.py
@@ -0,0 +1,41 @@
+
+from tkinter import *
+import requests
+from PIL import Image, ImageTk
+from io import BytesIO
+
+root = Tk()
+root.title('Online Image Viewer - Open online Images!')
+root.geometry('600x500')
+
+Label(root, text='', width=22, height=9).pack(side=TOP)
+
+label = Label(root, bg='white')
+label.pack(side='top')
+
+var = StringVar()
+entry = Entry(root, justify='center', textvariable=var)
+entry.focus_set()
+entry.place(x=200, y=40)
+
+web_imgs = []
+
+
+class WebImage:
+    def __init__(self, url):
+        u = requests.get(url)
+        self.image = ImageTk.PhotoImage(Image.open(BytesIO(u.content)))
+
+    def get(self):
+        return self.image
+
+
+def create_img():
+    image = WebImage(entry.get())
+    web_imgs.append(image)
+    label.config(image=image.get())
+
+
+go_btn = Button(root, text='Go', width=5, command=create_img)
+go_btn.place(x=270, y=100)
+root.mainloop()
